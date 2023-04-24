d21cdb53b4dae985efc2b66836b08deeec7add53
Jaspinder Singh <jaspinderkohli@gmail.com>
2023-04-24 14:53:40 -0230

    graphql apollo added and Callout page added

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,4 +1,7 @@
-'uce client'
+import { getClient } from "@/apollo-client";
+import CalloutCard from "@/components/CalloutCard";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
 type Props = {
     params: {
         city: string;
@@ -7,9 +10,47 @@ type Props = {
     };
 }
 
-function WeatherPage({params: {city, lat, long}} : Props) {
+async function WeatherPage({params: {city, lat, long}} : Props) {
+    const client = getClient()
+    const { data } = await client.query({
+        query: fetchWeatherQuery,
+        variables: {
+            current_weather: "true",
+            longitude: long,
+            latitude: lat,
+            timezone: 'GMT'
+        }
+    })
+
+    const results: Root = data.myQuery;
+
+    console.log(results);
+
   return (
-    <div>WeatherPage {city} {lat} {long}</div>
+    <div>
+        {/* Information Panel */}
+
+        <div>
+            <div className="p-5">
+                <div className="pb-5">
+                    <h2 className="text-xl font-bold">Todays overview</h2>
+                    <p className="text-sm text-gray-400">
+                        Last updated: {" "}
+                        {new Date(results.current_weather.time).toLocaleString()} 
+                        ({results.timezone})
+                    </p>
+                </div>
+
+                <div> 
+                    <CalloutCard
+                        message="It's currently 20°C in London"
+                    />
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
 
   )
 }
